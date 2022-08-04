cc7dc32cb609b1d35ad7b3b7ee828bbaa915bb63
jungjayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-08-04 11:37:56 +0900

    0804 과제 제출

---
+++ 0804/Main_bj_2164_카드2_서울_20반_정재영.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i = 1; i<=n; i++) {
+			q.offer(i);
+		}
+		
+		while(q.size() > 1) {
+			
+			q.remove();
+			int front = q.poll();
+			q.offer(front);
+			
+		}
+		
+		System.out.println(q.peek());
+		
+
+	}
+
+}
